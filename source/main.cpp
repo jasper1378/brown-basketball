27631b6717f1800f453607e4d0c519564654c9b2
Jasper Young <jasper.c.young@gmail.com>
2024-06-04 22:43:50 -0400

    Print time remaining during trials

--- source/main.cpp
+++ source/main.cpp
@@ -7,8 +7,9 @@
 #include "printing2.hpp"
 #include "scoring.hpp"
 
+#include "bits-and-bytes/timer/timer.hpp"
+
 #include <cstdlib>
-#include <iomanip>
 #include <iostream>
 #include <utility>
 
@@ -22,6 +23,7 @@ int main(int argc, char *argv[]) {
   auto d{database::read_database(argv[1])};
   analysis1::accum_state a1{d};
   analysis2::accum_state a2{};
+  bits_and_bytes::timer timer{};
   for (std::size_t i_trial{0}; i_trial < common::g_k_trial_count; ++i_trial) {
     // auto g{generation::generate_league(d, (generation::flags::draft_aware |
     //                                        generation::flags::position_aware))};
@@ -37,11 +39,18 @@ int main(int argc, char *argv[]) {
         scoring::return_value_type::team_ranks)>(
         scoring::score_league(g, scoring::return_value_type::team_ranks))};
     a2.add(std::make_pair(s_stats, s_ranks));
-    if ((i_trial % (common::g_k_trial_count / 100)) == 0) {
-      std::cerr << '\r'
-                << static_cast<int>(
-                       std::round(i_trial * 100.0 / common::g_k_trial_count))
-                << '%' << std::flush;
+    {
+      double percent_completed{static_cast<double>(i_trial) /
+                               common::g_k_trial_count};
+      if ((i_trial % (common::g_k_trial_count / 100)) == 0) {
+        std::cerr << "\33[2K\r"
+                  << static_cast<int>(std::round(percent_completed * 100))
+                  << "% completed, "
+                  << std::chrono::hh_mm_ss{(
+                         ((1 - percent_completed) / percent_completed) *
+                         timer.elapsed())}
+                  << " remaining" << std::flush;
+      }
     }
   }
   std::cerr << '\r';
